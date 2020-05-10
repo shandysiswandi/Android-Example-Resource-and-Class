8b94318d6f0b5f2bd106ca3ed51eaa56d9284ad9
sandybaik <sandys@bengkelbaik.id>
2020-05-10 22:13:52 +0700

    add utils constant

---
+++ java/utils/Constant.kt
@@ -0,0 +1,7 @@
+object Constant {
+    const val REQUEST_ID_MULTIPLE_PERMISSIONS: Int = 1
+    const val SPLASH_TIME_OUT: Long = 2000
+
+    const val DB_NAME = "movie_app_db"
+    const val DB_VERSION = 1
+}
