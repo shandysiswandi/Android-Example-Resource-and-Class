8d89807e1c2ebe8ec080740512493beca790285f
sandybaik <sandys@bengkelbaik.id>
2020-05-10 22:08:18 +0700

    add network ex: retrofit builder and api service

---
+++ java/network/ApiService.kt
@@ -0,0 +1,6 @@
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("users")
+    suspend fun getUsers(): List<User>
+}
---
+++ java/network/RetrofitBuilder.kt
@@ -0,0 +1,37 @@
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitBuilder {
+    private const val BASE_URL = "https://api.com/"
+
+    private fun createService(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(makeOkHttpClient())
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    private fun makeOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(makeLoggingInterceptor())
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
+    }
+
+    private fun makeLoggingInterceptor(): HttpLoggingInterceptor {
+        val logging = HttpLoggingInterceptor()
+        logging.level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        return logging
+    }
+
+    fun apiService(): ApiService {
+        return createService().create(ApiService::class.java)
+    }
+}
